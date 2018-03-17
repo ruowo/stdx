59c58136f3f3a362e8a440384846ff22da51357b
jetiny <86267344@qq.com>
2018-03-17 23:09:22 +0800

    fixed path

--- exec.js
+++ exec.js
@@ -1,6 +1,6 @@
 var path = require('path')
 var childProcess = require('child_process')
-let platform = process.platform
+var platform = process.platform
 switch (process.platform) {
   case 'win32':
     platform = 'win'
@@ -12,10 +12,10 @@ switch (process.platform) {
     platform = 'linux'
     break
 }
-const cmd = path.resolve(`./platform/${platform}/stdx${process.platform === 'win32' ? '.exe' : ''}`)
+var cmd = path.resolve(__dirname, `./platform/${platform}/stdx${process.platform === 'win32' ? '.exe' : ''}`)
 
 module.exports = function (args) {
-  let ps = childProcess.spawn(cmd, args, { stdio: 'inherit' })
+  var ps = childProcess.spawn(cmd, args, { stdio: 'inherit' })
   ps.on('close', (code) => {
     process.exit(code)
   })
