5df569aa95979d06dccf3d7b22b2297813f45f4a
jetiny <86267344@qq.com>
2018-03-23 08:50:35 +0800

    test musl

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -7,8 +7,7 @@ function downloadNodeSassAddions () {
     win32: 'win',
     darwin: 'mac'
   }
-  // , 'freebsd', 'linux_musl'
-  return Promise.all(['win32', 'darwin', 'linux'].map(it => {
+  return Promise.all(['win32', 'darwin', 'linux', 'freebsd', 'linux_musl'].map(it => {
     let dir = dirMap[it] || 'linux'
     let binPath = require('node-sass/lib/extensions').getBinaryPath()
     // \stdx\node_modules\node-sass\vendor\win32-x64-57\binding.node
