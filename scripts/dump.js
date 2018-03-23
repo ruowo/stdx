0457d5cbb1ca242d2282c09f3ef14fb47cd2fb4c
jetiny <86267344@qq.com>
2018-03-23 08:59:31 +0800

    dump sass info

---
+++ scripts/dump.js
@@ -0,0 +1,5 @@
+const exts = require('node-sass/lib/extensions.js')
+
+console.log('getBinaryName', exts.getBinaryName())
+console.log('getBinaryPath', exts.getBinaryPath())
+console.log('getBinaryUrl', exts.getBinaryUrl())
