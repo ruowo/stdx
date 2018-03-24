2a2d417726c2166a0f45e3b864fb2944a7413e5e
jetiny <86267344@qq.com>
2018-03-24 18:56:53 +0800

    fix

--- scripts/install.js
+++ scripts/install.js
@@ -6,14 +6,14 @@ const {name, version} = require('../package.json')
 exist(path.join(__dirname, '../platform'))
   .then((exist) => {
     if (!exist) {
-      let npm = process.platform === 'win32' ? 'npm.bat' : 'npm'
+      let npm = process.platform === 'win32' ? 'npm.cmd' : 'npm'
       let pkgMap = {
         win32: 'win',
         darwin: 'mac'
       }
       let pkg = pkgMap[process.platform] || 'linux'
       return spawn(npm, [
-        'install' , `${name}-${pkg}@${version}`
+        'install' , '--production', `${name}-${pkg}@${version}`
       ])
     }
   })
